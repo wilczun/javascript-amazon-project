d3a2e66a4d8e37bd17c1e7a5d41d06f59ff1ca9a
Pawel <hejawilk@gmail.com>
2025-12-22 04:30:39 +0100

    Delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,14 @@
-export const cart = [];
+export let cart = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1,
+  },
+];
 
-export function updateCartQuantity() {
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
-
-  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
-}
 export function addToCart(productId) {
   let matchingItem;
 
@@ -26,4 +26,14 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+  cart = newCart;
 }
